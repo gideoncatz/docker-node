d393871992e151d4abbd7b1b4c8f069eb3349597
Gideon Catz <gideonca@matrix.co.il>
2020-01-12 17:01:16 +0200

    addeed a JSON-based endpoint

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
 
+app.get('/api/items', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send([
+    {
+      itemId: 345,
+      itemName: 'Fusion drive'
+    },
+    {
+      itemId: 358,
+      itemName: 'Screwdriver'
+    }
+  ]);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.info(`Started app on port ${port}`);
